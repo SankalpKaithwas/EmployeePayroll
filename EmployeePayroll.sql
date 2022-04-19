93f66c6fe0c6579ec7caab45dfc111b99ad6ec4e
unknown <kaithwas.697@gmail.com>
2022-04-19 22:43:26 +0530

    Added Sql queries for Employee Payroll service Database

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -37,3 +37,33 @@ SELECT Gender, MIN(Salary) AS MIN_SALARY FROM Employee_Payroll GROUP BY Gender ;
 SELECT Gender, MAX(Salary) AS MAX_SALARY FROM Employee_Payroll GROUP BY Gender ;
 --number of males and females
 SELECT Gender, COUNT(Gender) AS GENDER_COUNT FROM Employee_Payroll GROUP BY Gender;
+
+--UC8
+ALTER TABLE Employee_Payroll ADD Addresses VARCHAR(250);
+ALTER TABLE Employee_Payroll ADD  Department VARCHAR(250);
+ALTER TABLE Employee_Payroll ADD CONSTRAINT DF_Address DEFAULT 'Address' FOR Addresses;
+
+--UC9
+ALTER TABLE Employee_Payroll ADD Basic_Pay BIGINT;
+ALTER TABLE Employee_Payroll ADD Deductions BIGINT;
+ALTER TABLE Employee_Payroll ADD Taxable_Pay BIGINT;
+ALTER TABLE Employee_Payroll ADD Income_Tax BIGINT;
+ALTER TABLE Employee_Payroll ADD Net_Pay BIGINT;
+
+
+--UC10
+INSERT INTO Employee_Payroll(Name,PhoneNumber,StartDate,Salary,Gender,Addresses,Department,Basic_Pay,Deductions,Taxable_Pay,Income_Tax,Net_Pay)
+VALUES ('Terissa',09345564,'12-12-20',347563,'F','Delhi','Sales',240000,34567,35612,12367,123456);
+
+CREATE TABLE Department(
+Dept_ID int NOT NULL,
+Name VARCHAR(100),
+Dept_Name VARCHAR(50),
+CONSTRAINT FK FOREIGN KEY (Dept_ID) REFERENCES Employee_Payroll(ID)
+);
+-- UC 11 ER DIAGRAM
+INSERT INTO Department(Dept_ID,Name,Dept_Name)
+VALUES(1,'Terrisa','Sales'),
+(1,'Terrisa','Marketing');
+
+SELECT * FROM Department;
